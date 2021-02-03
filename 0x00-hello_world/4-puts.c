feb59ee0410251d889b049d56b2a38b2627b6e82
Sebas93cay <sebas93cay@gmail.com>
2021-02-03 19:12:43 +0000

    Fix answer 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - funciont is the funcion the program start's running
+ * Return: the function always returns 0
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
