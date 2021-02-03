3c474d5ff13f367650c1619977e4f83d476dc908
Sebas93cay <sebas93cay@gmail.com>
2021-02-03 19:14:45 +0000

    Fix answer 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - funciont is the funcion the program start's running
+ * Return: the function always returns 0
+ **/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
