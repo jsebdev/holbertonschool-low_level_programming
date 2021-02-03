21b8c2017a38b3f59a493bc091c99caa0ed31145
Sebas93cay <sebas93cay@gmail.com>
2021-02-03 23:11:40 +0000

    Answer 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - funciont is the funcion the program start's running
+ * Return: the function always returns 0
+ **/
+int main(void)
+{
+	char *p ="and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+	//printf("%s", p);
+	//printf("%ld\n",sizeof(&p));
+	write(2, p, 58);
+	return (0);
+}
