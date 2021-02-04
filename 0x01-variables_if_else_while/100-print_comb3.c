474e997c29681b3de31e28bc002488c7af999815
Sebas93cay <sebas93cay@gmail.com>
2021-02-04 19:38:45 +0000

    Answer 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - funciont is the funcion the program start's running
+ * Return: the function always returns 0
+ **/
+int main(void)
+{
+	int i, u;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (u = 0; u <= 9; u++)
+		{
+			if (u > i)
+			{
+				putchar(i + '0');
+				putchar(u + '0');
+				if (i != 9 || u != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
