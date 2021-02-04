b76fe1b447c6e44237ce526cef4b32a1ed697424
Sebas93cay <sebas93cay@gmail.com>
2021-02-04 19:03:02 +0000

    Answer 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - funciont is the funcion the program start's running
+ * Return: the function always returns 0
+ **/
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; ++c)
+	{
+		putchar(c);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
