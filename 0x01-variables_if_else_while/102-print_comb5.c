da4d275d1f18c6c14c1b60f499ba7c7ecfa1c61e
Sebas93cay <sebas93cay@gmail.com>
2021-02-04 20:06:10 +0000

    Answer 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - funciont is the funcion the program start's running
+ * Return: the function always returns 0
+ **/
+int main(void)
+{
+	int x1, x2, y1, y2;
+
+	for (x1 = 0; x1 <= 9; x1++)
+	{
+		for (x2 = 0; x2 <= 9; x2++)
+		{
+			for (y1 = 0; y1 <= 9; y1++)
+			{
+				for (y2 = 0; y2 <= 9; y2++)
+				{
+					if (y1 > x1 || (y1 == x1 && y2 > x2))
+					{
+						putchar(x1 + '0');
+						putchar(x2 + '0');
+						putchar(' ');
+						putchar(y1 + '0');
+						putchar(y2 + '0');
+						if ((x1 != 9 ||  x2 != 8) || \
+						    (y1 != 9 || y2 != 9))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
