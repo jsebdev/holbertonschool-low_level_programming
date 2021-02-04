f4798dce6490f8958176ec5050143c9008749434
Sebas93cay <sebas93cay@gmail.com>
2021-02-04 18:42:37 +0000

    Answer 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - funciont is the funcion the program start's running
+ * Return: the function always returns 0
+ **/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
