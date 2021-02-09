66355de5db9f91c8af07c67fb799dd9c15097340
Sebas93cay <sebas93cay@gmail.com>
2021-02-09 21:58:44 +0000

    Fix 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-	int i = 0;
+	int i = 3;
 	long int num = 2, num1 = 1, aux;
 
 	printf("1, 2, ");
-	while (i < 50)
+	while (i <= 50)
 	{
 		aux = num;
 		num = num + num1;
 		num1 = aux;
 		i++;
 		printf("%ld", num);
-		if (i != 50)
+		if (i != 51)
 			printf(", ");
 	}
 	printf("\n");
