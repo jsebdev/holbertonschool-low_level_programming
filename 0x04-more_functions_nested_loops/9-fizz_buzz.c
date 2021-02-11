5ad91295f76a48a9859c766ffb81fe41d26d77c4
Sebas93cay <sebas93cay@gmail.com>
2021-02-11 16:50:15 +0000

    Answers 8 and 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/*
+ * main - buzzfizz test
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+				printf("FizzBuzz");
+			else
+				printf("Fizz");
+		}
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
