d939b165ac50ef319ff094909a707b2c609d9fc7
Sebas93cay <sebas93cay@gmail.com>
2021-02-16 20:21:02 +0000

    Fix 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,8 +7,7 @@
  */
 int _atoi(char *s)
 {
-	int len = 0, sign_num = 1, i = 0, no_num_yet = 0, res = 0, k = 0;
-	char ns[100];
+	int len = 0, sign_num = 1, i = 0, no_num_yet = 0, res = 0;
 
 	for (; *(s + len) != 0; len++)
 	{
@@ -27,22 +26,12 @@ int _atoi(char *s)
 		/*Take all the numbers into a new char[]*/
 		if (*(s + len) >= '0' && *(s + len) <= '9')
 		{
-			ns[i] = *(s + len);
+			res = res * 10 + *(s + len) - '0';
 			i++;
 			no_num_yet = 1;
 		}
 	}
-/*If there is no numbers the output is cero, we also put the 0 last byte*/
-	if (i != 0)
-		ns[i] = 0;
-	else
-	{
-		ns[0] = '0';
-		ns[1] = 0;
-		i = 1;
-	}
-	for (k = 0; k < i; k++)
-		res = res * 10 + ns[k] - '0';
+	/*If there is no numbers the output is cero, we also put the 0 last byte*/
 	if (sign_num < 0)
 		res = -res;
 	return (res);
