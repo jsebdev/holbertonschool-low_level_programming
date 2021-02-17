29c47ab7f5cc82b4631559c20b1b1f8e4c1fd505
Sebas93cay <sebas93cay@gmail.com>
2021-02-17 23:16:05 +0000

    Answer 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+	char password[200] = {0};
+
+	while (sum < 2772)
+	{
+		
+	}
+	return (0);
+}
