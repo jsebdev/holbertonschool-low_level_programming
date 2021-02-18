6b3854192d168512083d3bd8bb7301ffa06d2a67
Sebas93cay <sebas93cay@gmail.com>
2021-02-18 18:57:35 +0000

    Answer 8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * root13 - encodes a string using rot13.
+ * @s: string to encode
+ * Return: the string encoded
+ */
+
+char *rot13(char *s)
+{
+	char *lb = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *la = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0, k;
+
+	for (; *(s + i) != 0; i++)
+	{
+		for (k = 0; k < 52; k++)
+		{
+			if (*(s + i) == *(lb + k))
+			{
+				*(s + i) = *(la + k);
+				break;
+			}
+		}
+	}
+	return (s);
+}
