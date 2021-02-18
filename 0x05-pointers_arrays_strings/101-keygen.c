b02bda632b0c478f021aa8f30a35d75dacfc2fef
Sebas93cay <sebas93cay@gmail.com>
2021-02-18 06:13:44 +0000

    Fix 101

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <time.h>
 
 /**
  * main - main function
@@ -7,12 +8,38 @@
  */
 int main(void)
 {
-	int i, sum = 0;
+	int i = 0, sum = 0;
 	char password[200] = {0};
+	int new_caracter, overload;
+	int random = rand();
+	char *p;
 
-	while (sum < 2772)
+	srand(time(0));
+	do {
+		random = rand();
+		new_caracter = random % (126 - 32 + 1) + 32;
+		/* printf("%d es %c\n", new_caracter, new_caracter); */
+		password[i] = (char) new_caracter;
+		sum += new_caracter;
+		i++;
+	} while (sum < 2772);
+
+	overload = sum - 2772;
+	p = &password[i - 1];
+	/* printf("%s ultimo caracter: %c en memoria:%p\n", password, *p, p); */
+	while (1)
 	{
-		
+		if (*p > 32 + overload)
+		{
+			*p = *p - overload;
+			break;
+		}
+		else
+		{
+			p = p - 1;
+		}
 	}
+	/* printf("%s ultimo caracter: %c en memoria:%p\n", password, *p, p); */
+	printf("%s", password);
 	return (0);
 }
