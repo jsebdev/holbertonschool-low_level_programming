751523d0d6e797d51185b440df6e32a46a009d5b
Sebas93cay <sebas93cay@gmail.com>
2021-02-22 17:24:40 +0000

    Answer 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals of a square
+ * matrix of integers
+ * @a: matrix
+ * @size: size of matrix
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
