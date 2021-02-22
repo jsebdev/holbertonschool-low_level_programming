8a55f707b6a2733e51ac355a99cb6dd93289d383
Sebas93cay <sebas93cay@gmail.com>
2021-02-22 17:49:48 +0000

    Fix 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,21 +9,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, j = 0, n;
+	unsigned int i = 0, j = 0, n = 0, insegment = 0, match;
 
 	for (i = 0; *(s + i) != 0; i++)
 	{
+		match = 0;
 		for (j = 0; *(accept + j) != 0; j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
 				n++;
-				/* printf("%c = %c ? ", *(s + i), */
-				/*        *(accept + j)); */
-				/* printf("ahora n es mayor n=%d i=%d j=%d\n", */
-				       /* n, i, j); */
+				insegment = 1;
+				match = 1;
 				break;
 			}
+		}
+		if (insegment && !match)
+		{
+			break;
 		}
 	}
 	return (n);
