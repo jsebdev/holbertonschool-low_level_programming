c32bd9fa60613b92548c71ecc82f75afd041f8e8
Sebas93cay <sebas93cay@gmail.com>
2021-02-22 17:27:14 +0000

    Answer 9

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
 
 /**
- * print_diagsums - prints the sum of the two diagonals of a square
- * matrix of integers
- * @a: matrix
- * @size: size of matrix
+ * set_string - sets the value of a pointer to a char
+ * @s: char to set
+ * @to: pointer
  * Return: nothing
  */
 void set_string(char **s, char *to)
