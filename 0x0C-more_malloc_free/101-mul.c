f2ac88aa85689216ed692f33e6fa3c3915c115d5
Sebas93cay <sebas93cay@gmail.com>
2021-03-05 04:54:43 +0000

    Fix 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -83,12 +83,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			len2++;
 	}
 	len2 = (len2 > (int) n) ? (int) n : len2;
-	/* if (n == 7) */
-	/* { */
-	/* 	printf("Inside nconcat\n"); */
-	/* 	printf("s1 = %s\nlen1 = %d\ns2 = %s\nlen2 = %d\n", s1, */
-	/* 	       len1, s2, len2); */
-	/* } */
 	nstr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (nstr == NULL)
 		return (NULL);
@@ -97,12 +91,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (; i < len1 + len2; i++)
 		*(nstr + i) = *(s2 + i - len1);
 	*(nstr + i) = '\0';
-
-	/* if (n == 7) */
-	/* { */
-	/* 	printf("nstr = %s\n", nstr); */
-	/* } */
-
 	return (nstr);
 }
 
@@ -239,9 +227,7 @@ int main(int argc, char **argv)
 	{
 		*(facts + 0) = mul_digit(argv[1], len1, argv[2][len2 - i - 1],
 					 facts[0], facts_size + 0);
-		/* printf("mul %d - %s\n", i, facts[0]); */
 		*(facts + 0) = add_0s(facts[0], facts_size + 0, i);
-		/* printf("mul %d - %s\n", i, facts[0]); */
 		facts[2] = infinite_add(facts[0], facts[1], facts[2],
 					facts_size + 2);
 		switch_pointers(facts + 1, facts + 2, &facts_size[1],
@@ -286,28 +272,14 @@ char *add_0s(char *num, int *len, int c)
 	}
 	*(ceros + i) = 0;
 
-	/* if (c == 7) */
-	/* { */
-	/* 	printf("c = %d\nnum = %s\nlen = %d\n", c, num, *len); */
-	/* } */
 	num = _realloc(num, *len, *len + c);
 	if (num == NULL)
 	{
 		return (NULL);
 	}
 	*len = *len + c;
-	/* if (c == 7) */
-	/* { */
-	/* 	printf("c = %d\nnum = %s\nlen = %d\n", c, num, *len); */
-	/* } */
 
 	res = string_nconcat(num, ceros, c);
-	/* if (c == 7) */
-	/* { */
-	/* 	printf("c = %d\nnum = %s\nceros = %s\nres = %s\n", c, num, */
-	/* 	       ceros, res); */
-	/* } */
-
 	free(num);
 	free(ceros);
 	return (res);
