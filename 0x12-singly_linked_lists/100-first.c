5d1cc2fc137f05683f5a9f821ee8762d843666a5
Sebas93cay <sebas93cay@gmail.com>
2021-03-18 18:42:57 +0000

    Answer 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+
+/**
+ * premain - functions which executes before the main function
+ * Return: nothing
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,%s",
+	       "\nI bore my house upon my back!\n");
+}
