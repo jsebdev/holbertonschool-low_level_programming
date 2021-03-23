ed18a2517e1ad789245a3cf1486953ac1e550b3b
Sebastian Asus Elementary <sebas93cay@gmail.com>
2021-03-23 16:24:06 +0000

    Change 103 answer to use same listint_t as the list to compare with the current node

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,60 +2,22 @@
 
 
 /**
- * free_list_adr2 - frees a list
- * @head: list to free
- * Return: nothing
- */
-void free_list_adr2(list_adr *head)
-{
-	if (head == NULL)
-		return;
-	if (head->next != NULL)
-	{
-		free_list_adr2(head->next);
-	}
-	free(head);
-}
-
-/**
- * add_nodeadr2 - adds a new node at the beginning of a listint_t list
- * @head: list
- * @adr: address of node
- * Return: the address of the new element, or NULL if it failed
- */
-list_adr *add_nodeadr2(list_adr **head, const listint_t *adr)
-{
-	list_adr *tmp_head = NULL;
-
-	tmp_head = malloc(sizeof(list_adr));
-	if (tmp_head)
-	{
-		tmp_head->next = *head;
-		tmp_head->adr = adr;
-		*head = tmp_head;
-		return (tmp_head);
-	}
-	return (NULL);
-}
-
-/**
  * check_adrs2 - check if address is already in list
  * @adr: address to compare
  * @adrs: list of arrays to compare
  * Return: 1 if addres is not yet in the list, 0 otherwise
  */
-int check_adrs2(const listint_t *adr, list_adr *adrs)
+int check_adrs2(const listint_t *adr, listint_t *head, int nodes)
 {
-	if (adrs == NULL)
-		return (1);
-	if ((void *)adr == (void *)(adrs->adr))
-		return (0);
-	else if (adrs->next)
-		return (check_adrs2(adr, adrs->next));
+	if (nodes > 0)
+	{
+		if (adr == head)
+			return (0);
+		else if (head->next)
+			return (check_adrs2(adr, head->next, nodes - 1));
+	}
 	return (1);
 }
-
-
 
 
 /**
@@ -66,7 +28,7 @@ int check_adrs2(const listint_t *adr, list_adr *adrs)
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	list_adr *addrs = NULL;
+	int nodes = 0;
 	listint_t *tmp_node;
 
 	if (head == NULL)
@@ -75,18 +37,16 @@ listint_t *find_listint_loop(listint_t *head)
 	tmp_node = head;
 	while (tmp_node)
 	{
-		if (check_adrs2(tmp_node, addrs))
+		if (check_adrs2(tmp_node, head, nodes))
 		{
-			add_nodeadr2(&addrs, tmp_node);
+			nodes++;
 			tmp_node = tmp_node->next;
 		}
 		else
 		{
-			free_list_adr2(addrs);
 			return (tmp_node);
 		}
 	}
-	free_list_adr2(addrs);
 	return (NULL);
 }
 
