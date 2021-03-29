8bffdc2897f26cc1c8db778905155986ee9efa4d
Sebas93cay <sebas93cay@gmail.com>
2021-03-29 20:18:18 +0000

    Also truncate file in answer 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_d = open(filename, O_CREAT | O_RDWR, 0600);
+	file_d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file_d == -1)
 		return (-1);
 
