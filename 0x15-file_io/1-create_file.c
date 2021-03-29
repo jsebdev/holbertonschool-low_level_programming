9c366fdcf24da873e08133a4b69ee8d84d9a3ca3
Sebas93cay <sebas93cay@gmail.com>
2021-03-29 20:13:17 +0000

    Answer 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+# include "holberton.h"
+
+/**
+ * create_file - create a file
+ * @filename: file
+ * @text_content: text to copy on file
+ * Return: return 1 if success -1 otherwise
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_d, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_d = open(filename, O_CREAT | O_RDWR, 0600);
+	if (file_d == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+
+		if (write(file_d, text_content, len) == -1)
+			return (-1);
+	}
+	return (1);
+}
