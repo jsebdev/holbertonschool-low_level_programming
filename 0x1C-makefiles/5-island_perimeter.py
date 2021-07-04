1c92aa6cd861db1a58e692645bd13dcca19168c9
Seb Asus VS <sebas93cay@gmail.com>
2021-07-04 18:09:56 -0500

    Fix 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -21,17 +21,17 @@ def island_perimeter(grid):
         for j, land in enumerate(row):
             if land:
                 if j - 1 < 0:
-                    perimeter += -row[j-1] + 1
-                else:
                     perimeter += 1
+                else:
+                    perimeter += -row[j-1] + 1
                 try:
                     perimeter += -row[j+1] + 1
                 except IndexError:
                     perimeter += 1
                 if i - 1 < 0:
-                    perimeter += -grid[i-1][j] + 1
-                else:
                     perimeter += 1
+                else:
+                    perimeter += -grid[i-1][j] + 1
                 try:
                     perimeter += -grid[i+1][j] + 1
                 except IndexError:
